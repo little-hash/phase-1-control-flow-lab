0ff2918ae3d12462da39f265ca94c438a97e6620
little-hash <ezraraddai166@gmail.com>
2024-09-26 13:12:02 +0300

    commitfirts

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Feet) {
   // Write your code here!
+  let MaxFeet = 2500
+  if(Feet <= 400){
+    return 'This one is on me!';
+  }
+  else if(Feet>=400 && Feet<=2000){
+    return 'That will be twenty bucks.'
+  }
+  else if(Feet>2500){
+  return 'No can do.'
+  }
+  else if(Feet>2000){
+    return 'I will gladly take your thirty bucks.'
+  }    
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
+  }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+
 }
+ 
+
+
+   
+
