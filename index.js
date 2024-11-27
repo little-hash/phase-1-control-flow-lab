d40d7e26e35ef338e4c6f44a316a7812916528a5
Ezra <ezraraddai166@gmail.com>
2024-11-27 14:04:42 +0300

    firstcommit

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!"
+  } else if (feet >= 400 && feet <= 2000) {
+    return "That will be twenty bucks."
+  } if (feet >= 2000 && feet <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (feet > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(City) {
   // Write your code here!
+  if (City === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
